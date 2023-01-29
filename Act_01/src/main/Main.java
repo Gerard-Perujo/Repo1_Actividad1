ead49fc48fa0e04fc9b896f5aefd1543fc93ed87
Gerard.Perujo <ger.pe85@gmail.com>
2023-01-29 15:27:31 +0100

    creado llamarNombre en main

--- Act_01/src/main/Main.java
+++ Act_01/src/main/Main.java
@@ -1,11 +1,21 @@
 package main;
 
+import gerard.Gerard;
+
 public class Main {
 
 	public static void main(String[] args) {
 	
 		System.out.println("Actividad1 de Entornos");
 		System.out.println("----------------------");
+		
+		//creo el objeto Gerard//
+		
+		Gerard ger = new Gerard("Gerard");
+		
+		//utilizo el metodo llamar para hacer el syso//
+		
+		System.out.println("hola soy " + ger.llamarNombre());
 
 	}
 
