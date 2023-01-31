2b28dc02727f41e2300c3683db11820de05c6c5e
Maria Isabel <contacto.mims@gmail.com>
2023-01-31 21:20:05 +0100

    Cambios en gerard.java

--- Act_01/src/gerard/Gerard.java
+++ Act_01/src/gerard/Gerard.java
@@ -2,16 +2,24 @@ package gerard;
 
 public class Gerard {
 		
-		private String nombre, hobbi1, hobbi2, hobbi3;
+		private String nombre, hobbi1, hobbi2, hobbi3, comida1, comida2, comida3;
 		
 
 
-		public Gerard(String nombre, String hobbi1, String hobbi2, String hobbi3) {
+		
+		public Gerard(String nombre, String hobbi1, String hobbi2, String hobbi3, String comida1, String comida2,
+				String comida3) {
 			super();
 			this.nombre = nombre;
 			this.hobbi1 = hobbi1;
 			this.hobbi2 = hobbi2;
 			this.hobbi3 = hobbi3;
+			this.comida1 = comida1;
+			this.comida2 = comida2;
+			this.comida3 = comida3;
+		}
+
+
 		}
 
 
@@ -72,10 +80,49 @@ public class Gerard {
 
 		
 	
+public String getComida1() {
+			return comida1;
+		}
+
+
+
+		public void setComida1(String comida1) {
+			this.comida1 = comida1;
+		}
+
+
+
+		public String getComida2() {
+			return comida2;
+		}
+
+
+
+		public void setComida2(String comida2) {
+			this.comida2 = comida2;
+		}
+
+
+
+		public String getComida3() {
+			return comida3;
+		}
+
+
+
+		public void setComida3(String comida3) {
+			this.comida3 = comida3;
+		}
+
+
+
+
+
+
 @Override
 		public String toString() {
 			return "Gerard [nombre=" + nombre + ", hobbi1=" + hobbi1 + ", hobbi2=" + hobbi2 + ", hobbi3=" + hobbi3
-					+ "]";
+					+ ", comida1=" + comida1 + ", comida2=" + comida2 + ", comida3=" + comida3 + "]";
 		}
 
 
@@ -87,5 +134,8 @@ public String llamarNombre() {
 
 public String tiposHobies() {
 	return hobbi1 + hobbi2 + hobbi3;
+}
+public String llamarComidas () {
+	return comida1 + ", " + comida2 + "y " + comida3;
 }
 }
