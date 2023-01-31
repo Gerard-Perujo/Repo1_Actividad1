b39921681905cdb5b5c9aebe73385fbfa47a0ffa
Noelia.villgar <noeliavg_16_diez@hotmail.com>
2023-01-31 18:27:41 +0100

    hobbies

--- Act_01/src/Noelia/Noelia.java
+++ Act_01/src/Noelia/Noelia.java
@@ -2,30 +2,92 @@ package Noelia;
 
 public class Noelia {
 	
-	private String nombre;
-
+	private String nombre, hobbi1, hobbi2, hobbi3; 
+	
+	public Noelia (String nombre, String hobbi1, String hobbi2, String hobbi3) {
+		super();
+		this.nombre = nombre;
+		this.hobbi1 = hobbi1;
+		this.hobbi2 = hobbi2;
+		this.hobbi3 = hobbi3;
+	}
 	
 	public Noelia() {
 		super();
 	}
 
-	public Noelia(String nombre) {
-		super();
-		this.nombre = nombre;
-	}
+
 
 	public String getNombre() {
 		return nombre;
 	}
 
+
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	
+	public String getHobbi1() {
+		return hobbi1;
+	}
 
-	@Override
+
+
+	public void setHobbi1(String hobbi1) {
+		this.hobbi1 = hobbi1;
+	}
+
+
+
+	public String getHobbi2() {
+		return hobbi2;
+	}
+
+
+
+	public void setHobbi2(String hobbi2) {
+		this.hobbi2 = hobbi2;
+	}
+
+
+
+	public String getHobbi3() {
+		return hobbi3;
+	}
+
+
+
+	public void setHobbi3(String hobbi3) {
+		this.hobbi3 = hobbi3;
+	}
+
+
+
+
+	
+
+@Override
 	public String toString() {
-		return "noelia [nombre=" + nombre + "]";
-	} 
+		return "Noelia [nombre=" + nombre + ", hobbi1=" + hobbi1 + ", hobbi2=" + hobbi2 + ", hobbi3=" + hobbi3
+				+ "]";
+	}
+
+
+
+	public String llamarNombre() {
+		return nombre;
+	}
+
+
+	public String tiposHobies() {
+		return hobbi1 + hobbi2 + hobbi3;
+}
+}
+
+	
+	
 	
 public String llamarNombre() {
 	return nombre; 
