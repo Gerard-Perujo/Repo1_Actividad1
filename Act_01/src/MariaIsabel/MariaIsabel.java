66ec6369d410e4df18a92b1df17cbec15dab1bce
Maria Isabel <contacto.mims@gmail.com>
2023-02-02 19:44:54 +0100

    Quitar comidas para solucionar problema previo con duplicidades

--- Act_01/src/MariaIsabel/MariaIsabel.java
+++ Act_01/src/MariaIsabel/MariaIsabel.java
@@ -1,108 +1,74 @@
 package MariaIsabel;
 
 public class MariaIsabel {
+	//Atributos privados
+	private String nombre, hobby1, hobby2, hobby3;
 	
-	private String nombre, hobby1, hobby2, hobby3, comida1, comida2, comida3;
-	//Constructores
+	//Constructores, uno con todos los parametros y otro sin nada
+	public MariaIsabel() {
+		super();
+	}
 
-	public MariaIsabel(String nombre, String hobby1, String hobby2, String hobby3, String comida1, String comida2,
-			String comida3) {
+	public MariaIsabel(String nombre, String hobby1, String hobby2, String hobby3) {
 		super();
 		this.nombre = nombre;
 		this.hobby1 = hobby1;
 		this.hobby2 = hobby2;
 		this.hobby3 = hobby3;
-		this.comida1 = comida1;
-		this.comida2 = comida2;
-		this.comida3 = comida3;
 	}
-
-	public MariaIsabel() {
-		super();
-	}
+	
 	
 	//Getters and setters
 
-	public String getNombre() {
+		public String getNombre() {
 		return nombre;
 	}
-	public String getComida1() {
-		return comida1;
-	}
-
-	public void setComida1(String comida1) {
-		this.comida1 = comida1;
-	}
-
-
-	public String getComida2() {
-		return comida2;
-	}
-
-
-	public void setComida2(String comida2) {
-		this.comida2 = comida2;
-	}
-
-
-	public String getComida3() {
-		return comida3;
-	}
-
-
-	public void setComida3(String comida3) {
-		this.comida3 = comida3;
-	}
-
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getHobby1() {
 		return hobby1;
 	}
+
 	public void setHobby1(String hobby1) {
 		this.hobby1 = hobby1;
 	}
+
 	public String getHobby2() {
 		return hobby2;
 	}
+
 	public void setHobby2(String hobby2) {
 		this.hobby2 = hobby2;
 	}
+
 	public String getHobby3() {
 		return hobby3;
 	}
+
 	public void setHobby3(String hobby3) {
 		this.hobby3 = hobby3;
 	}
-
 	
-	//Metodo toString
-	@Override
+	// Redefinir metodo de Object
+
+		@Override
 	public String toString() {
 		return "MariaIsabel [nombre=" + nombre + ", hobby1=" + hobby1 + ", hobby2=" + hobby2 + ", hobby3=" + hobby3
-				+ ", comida1=" + comida1 + ", comida2=" + comida2 + ", comida3=" + comida3 + "]";
+				+ "]";
 	}
-	
-
-	
-	
-	//Metodos propios
 
 		//Metodos propios
-
 		public String llamarNombre() {
 			return nombre;
 		}
-
 
 		public String misHobbys() {
 			return hobby1+ ", " + hobby2+ " y " + hobby3;
 		}
 		
-		public String comidasFavoritas() {
-			return comida1 + ", " + comida2 + " y " + comida3;
-		}
+
 
 }
